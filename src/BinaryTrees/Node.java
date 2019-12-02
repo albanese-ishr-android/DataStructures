8e7335194562a3cda9368e2b9a2824a2efd40cb9
tony-albanese <tony.albanese2000@gmail.com>
2019-12-02 19:32:37 +0100

    Initial commit.

---
+++ src/BinaryTrees/Node.java
@@ -0,0 +1,13 @@
+package BinaryTrees;
+
+public class Node {
+    Node left;
+    Node right;
+    int data;
+
+    Node(int data) {
+        left = null;
+        right = null;
+        this.data = data;
+    }
+}
