b2ffdcf676ea48afe1d23503c72d217cbb3e2f0e
tony-albanese <tony.albanese2000@gmail.com>
2019-12-02 20:28:08 +0100

    Added methods for BT.

--- src/BinaryTrees/BinaryTree.java
+++ src/BinaryTrees/BinaryTree.java
@@ -2,4 +2,70 @@ package BinaryTrees;
 
 public class BinaryTree {
 
+    public Node root;
+
+    BinaryTree() {
+        root = null;
+    }
+
+    private Node addRecursive(Node current, int value) {
+        if (current == null) {
+            return new Node(value);
+        }
+
+        if (value < current.data) {
+            current.left = addRecursive(current.left, value);
+        } else if (value > current.data) {
+            current.right = addRecursive(current.right, value);
+        } else {
+            // value already exists
+            return current;
+        }
+
+        return current;
+    }
+
+    private boolean containsNodeRecursive(Node current, int value) {
+        if (current == null) {
+            return false;
+        }
+        if (value == current.data) {
+            return true;
+        }
+        return value < current.data
+                ? containsNodeRecursive(current.left, value)
+                : containsNodeRecursive(current.right, value);
+    }
+
+    public void add(int value) {
+        root = addRecursive(root, value);
+    }
+
+    public boolean containsNode(int value) {
+        return containsNodeRecursive(root, value);
+    }
+
+    public void traverseInOrder(Node node) {
+        if (node != null) {
+            traverseInOrder(node.left);
+            System.out.print(" " + node.data);
+            traverseInOrder(node.right);
+        }
+    }
+
+    public void traversePreOrder(Node node) {
+        if (node != null) {
+            System.out.print(" " + node.data);
+            traversePreOrder(node.left);
+            traversePreOrder(node.right);
+        }
+    }
+
+    public void traversePostOrder(Node node) {
+        if (node != null) {
+            traversePostOrder(node.left);
+            traversePostOrder(node.right);
+            System.out.print(" " + node.data);
+        }
+    }
 }
--- src/BinaryTrees/BinaryTreeDriver.java
+++ src/BinaryTrees/BinaryTreeDriver.java
@@ -2,4 +2,27 @@ package BinaryTrees;
 
 public class BinaryTreeDriver {
 
+    public static void main(String[] args) {
+        BinaryTree tree = createBinaryTree();
+        tree.traverseInOrder(tree.root);
+        System.out.println("");
+        tree.traversePostOrder(tree.root);
+
+        Node testNode = tree.root;
+    }
+
+
+    public static BinaryTree createBinaryTree() {
+        BinaryTree bt = new BinaryTree();
+
+        bt.add(6);
+        bt.add(4);
+        bt.add(8);
+        bt.add(3);
+        bt.add(5);
+        bt.add(7);
+        bt.add(9);
+
+        return bt;
+    }
 }
